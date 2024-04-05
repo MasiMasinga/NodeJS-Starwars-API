8cf8802b4d1f927f073f80d6c4dedab20882edba
Masi <51009658+MasiMasinga@users.noreply.github.com>
2024-04-05 03:11:01 +0200

    fix error message for invalid authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,15 +5,16 @@ dotenv.config();
 const authenticate = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        return res.status(401).send('No token provided');
+        return res.status(401).send('Authorization token is missing');
     }
+
     const token = authHeader.split(' ')[1];
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET);
         req.userId = payload.userId;
         next();
     } catch (err) {
-        res.status(401).send('Invalid token');
+       return res.status(401).send('Invalid authorization token');
     }
 };
 
